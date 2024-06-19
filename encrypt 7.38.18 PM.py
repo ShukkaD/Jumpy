f65dbb4eaa36111173783c01cd2bc8c8d5430e5f
landocoderissian <126412873+landocoderissian@users.noreply.github.com>
2024-06-19 19:38:49 -0400

    Mac App included
    Mac app 'Jumpy.app'

---
+++ encrypt 7.38.18 PM.py
@@ -0,0 +1,64 @@
+import os
+from cryptography.fernet import Fernet
+
+key = Fernet.generate_key()
+
+
+class Encrypt:
+    def __init__(self, file):
+        global key
+        base_dir = os.path.dirname(file)
+        self.file = os.path.join(base_dir, file)
+        self.key = key
+        with open(os.path.join(base_dir, 'key'), 'wb') as filekey:
+            filekey.write(self.key)
+        self.fernet = Fernet(key)
+        with open(self.file, 'rb') as file:
+            original = file.read()
+
+        encrypted = self.fernet.encrypt(original)
+
+        with open(self.file, 'wb') as encrypted_file:
+            encrypted_file.write(encrypted)
+
+
+class FileEncryptor():
+    def __init__(self, key):
+        self.fernet = Fernet(key)
+
+    def is_encrypted(self, file_path):
+        try:
+            base_dir = os.path.dirname(file_path)
+            file_path = os.path.join(base_dir, file_path)
+            with open(file_path, 'rb') as enc_file:
+                encrypted = enc_file.read()
+            self.fernet.decrypt(encrypted)
+            return True
+        except Exception as e:
+            # If an exception occurs during decryption, the file is not encrypted.
+            return False
+
+
+class Decrypt:
+    def __init__(self, file, key_file):
+        base_dir = os.path.dirname(file)
+        self.file = os.path.join(base_dir, file)
+        self.key_file = os.path.join(base_dir, key_file)
+
+        # Load the key from the key file
+        with open(self.key_file, 'rb') as keyfile:
+            self.key = keyfile.read()
+
+        # Using the key for decryption
+        self.fernet = Fernet(self.key)
+
+        # Opening the encrypted file
+        with open(self.file, 'rb') as enc_file:
+            encrypted = enc_file.read()
+
+        # Decrypting the file
+        decrypted = self.fernet.decrypt(encrypted)
+
+        # Opening the file in write mode and writing the decrypted data
+        with open(self.file, 'wb') as dec_file:
+            dec_file.write(decrypted)
